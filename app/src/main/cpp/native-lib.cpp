d7d5603bbe4be1702cc450cf282120e637b0251c
chalilayang <chalilayang@gmail.com>
2020-10-13 16:56:45 +0800

    update

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -5,6 +5,6 @@ extern "C" JNIEXPORT jstring JNICALL
 Java_com_miui_ndkapplication_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "Hello from Cddd++";
     return env->NewStringUTF(hello.c_str());
 }
